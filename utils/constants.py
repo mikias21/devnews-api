820ee6c1d1f5ef741f50f64aac562c99f31adf2f
mikias21 <mikiasmesfinA0123@gmail.com>
2024-02-05 19:06:45 +0800

    Updated web scrapper to scrap from severl medium.com tags

---
+++ utils/constants.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class MediumTags(Enum):
+    TAGS = [
+        "blockchain",
+        "programming-languages",
+        "devops",
+        "gaming",
+        "programming",
+        "software-development",
+        "technology",
+        "web-development",
+        "javascript",
+        "python",
+        "coding",
+        "machine-learning",
+        "bug-bounty",
+        "cybersecurity",
+        "hacking",
+        "infosec",
+        "penetration-testing"
+    ] 
